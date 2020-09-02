6587acd74bb72a561857a474084e1169e1bdcee6
marcellita <mteliceskidalpino@gmail.com>
2020-09-02 17:31:07 -0300

    projeto 2

--- src/data.js
+++ src/data.js
@@ -36,20 +36,3 @@ export const calcType = (filterPokemon, allPokemon) => {
 }
 
 
-
-export const orderPokedex = (myPokemon, id, alfaOrder) => {
-    if (alfaOrder == "pokedex") {
-        return myPokemon.sort((a, b) => {
-            if (b.id < a.id) {
-                return 1;
-            } else {
-                return -1;
-            }
-        })
-    }
-}
-
-export const calcType = (filterPokemon, allPokemon) => {
-    let percentage = filterPokemon * 100 / allPokemon;
-    return percentage;
-}
